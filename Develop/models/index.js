60dcdd5d1772170d6ad30deb5d9a3ea7072fc46b
Misachka <136197530+Misachka@users.noreply.github.com>
2023-10-29 15:32:20 -0400

    added categories and index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: "CASCADE"
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: "CASCADE"
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags
+Product.belongsToMany(Tag, {
+  foreignKey: "product_id",
+  through: ProductTag,
+  onDelete: "CASCADE"
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products 
+Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
+  through: ProductTag,
+  onDelete: "CASCADE"
+});
 
 module.exports = {
   Product,
